f718c05701490f51736a5458e65a200a54549642
Scheffer <vscheffer@narwalsistemas.com.br>
2019-05-15 16:18:25 -0300

    personalizações

--- DanfeSharp/DanfePagina.cs
+++ DanfeSharp/DanfePagina.cs
@@ -41,7 +41,7 @@ namespace DanfeSharp
 
         public void DesenharCreditos()
         {
-            Gfx.DrawString("Impresso com DanfeSharp", RetanguloCreditos, Danfe.EstiloPadrao.CriarFonteItalico(6), AlinhamentoHorizontal.Direita);
+            Gfx.DrawString("Impresso com NARWAL TECNOLOGIA LTDA", RetanguloCreditos, Danfe.EstiloPadrao.CriarFonteItalico(6), AlinhamentoHorizontal.Direita);
         }
 
         private void DesenharCanhoto()
